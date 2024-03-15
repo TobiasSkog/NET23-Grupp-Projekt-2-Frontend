fa51039f8b9fccbc8bdec83e072e7dde629e795d
Tobias Skog <t_ott_e@hotmail.com>
2024-03-15 11:57:27 +0100

    update to custom modal

--- src/Components/CustomModal/CustomModal.jsx
+++ src/Components/CustomModal/CustomModal.jsx
@@ -15,14 +15,14 @@ export default function CustomModal({ isOpen, onClose, children }) {
 	return (
 		<>
 			{isVisible && (
-				<div className="custom-modal-overlay">
+				<section className="custom-modal-overlay">
 					<div className="custom-modal-content">
 						<button className="custom-modal-close-button" onClick={handleClose}>
 							&times;
 						</button>
 						{children}
 					</div>
-				</div>
+				</section>
 			)}
 		</>
 	);
